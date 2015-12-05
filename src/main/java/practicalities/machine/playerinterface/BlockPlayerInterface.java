c7f84c3611a708a62742c47c77cac8d6cc9347dc
Joe Kaiser <joekaiser@live.com>
2015-12-04 20:34:36 -0800

    playerinterface texture changes

--- src/main/java/practicalities/machine/playerinterface/BlockPlayerInterface.java
+++ src/main/java/practicalities/machine/playerinterface/BlockPlayerInterface.java
@@ -1,21 +1,46 @@
 package practicalities.machine.playerinterface;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
-import practicalities.PracticalitiesMod;
 import practicalities.blocks.BlockBase;
 
 public class BlockPlayerInterface extends BlockBase implements ITileEntityProvider {
 
+	@SideOnly(Side.CLIENT)
+	protected IIcon icon_top;
+	@SideOnly(Side.CLIENT)
+	protected IIcon icon_side;
+	
 	public BlockPlayerInterface() {
 		super(Material.iron, "playerinterface", 1, null);
 		setStepSound(soundTypeMetal);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerBlockIcons(IIconRegister ir) {
+		this.icon_top = ir.registerIcon(getTexture("playerinterface/playerinterface_top"));
+		this.icon_side = ir.registerIcon(getTexture("playerinterface/playerinterface"));
+	}
+
+	@Override
+	public IIcon getIcon(int side, int meta) {
+
+		if (side == 1 || side==0) {
+			return this.icon_top;
+		}
+
+		return this.icon_side;
 	}
 	
 	@Override
